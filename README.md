ec00de187d3e5dc92eb2e3e208b744af516c3bcd
sszachow <sszachow@gmail.com>
2014-01-03 16:55:46 +0100

    added Color class in gen_photos.py; updated README.md

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Simple js app for creating vcf files.
 TODO:
 * add license
 * add slider for percentage of pictures
-* write nice color gen class in gen_photos.py
 * think about smarter photos implementation (iCloud fails if using embedded PNG; Gmail doesn't accept vcfs with photos as URLs; use local library for creating images)
+* add contrast generation in Color class
+* add address field generation in main.js
--- py/gen_photos.py
+++ py/gen_photos.py
@@ -3,33 +3,50 @@ from base64 import b64encode
 import string
 import random
 
-size = 24
-type = 'jpeg'
 images = 10
+size = 64
+type = 'jpeg'
 file_path = "../js/photos.js"
+chars = string.ascii_letters + string.digits
 
-def id_generator(size = 2, chars = string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for x in range(size))
+def gen_id(length = 2, chars = chars):
+    return ''.join(random.choice(chars) for x in range(length))
 
-url = "http://dummyimage.com/%d/000/fff.%s&text=%s" % (size, type, id_generator())
 
-f = open(file_path, 'w');
-f.write("var photosArr = [\n")
+def gen_js_file(images, size, type, file_path):
+    with open(file_path, 'w') as f:
+        f.write("var photosArr = [\n")
+        c = Color()
 
-for i in range(0, images):
-    url = "http://dummyimage.com/%d/000/fff.%s&text=%s" % (size, type, id_generator())
-    print url
-    r = requests.get(url)
+        for i in range(0, images):
+            url = "http://dummyimage.com/%d/%s/%s.%s&text=%s" % (size, c.rnd_color(), c.rnd_color(), type, gen_id())
+            print url
+            r = requests.get(url)
+            
+            temp = b64encode(r.content) if r.status_code == 200 else ""
+            f.write("\t\"%s\"" % temp)
+            f.write(",\n") if i < (images - 1) else f.write("\n];\n")
 
-    if r.status_code == 200:
-        temp = b64encode(r.content)
-    else:
-        temp = ""
+
+class Color(object):
+    def __init__(self, r = 0, g = 0, b = 0, max_color = 0xf):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.max_color = max_color
+
+    def as_string(self):
+        return hex(self.r)[2:] + hex(self.g)[2:] + hex(self.b)[2:]
     
-    f.write("\t\"%s\"" % temp)
-    if i < (images - 1):
-        f.write(",\n")
-    else:
-        f.write("\n];\n")
+    def average_color(self):
+        return float(self.r + self.g + self.b) / 3
+    
+    def rnd_color(self):
+        self.r = random.randint(0, self.max_color)
+        self.g = random.randint(0, self.max_color)
+        self.b = random.randint(0, self.max_color)
+        return self.as_string()
 
-f.close()
+
+if __name__ == "__main__":
+    gen_js_file(images, size, type, file_path)
