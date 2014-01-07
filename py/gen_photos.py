6c5ca81ca6174a5da4d258c19fc27932da21b1d1
sszachow <sszachow@gmail.com>
2014-01-07 18:39:21 +0100

    minor changes in gen_photos.py

--- py/gen_photos.py
+++ py/gen_photos.py
@@ -6,7 +6,6 @@ import random
 
 images = 10
 size = 64
-type = 'jpeg'
 types = ['jpeg', 'png', 'gif']
 file_path = "../js/photos.js"
 chars = string.ascii_letters + string.digits
@@ -33,7 +32,7 @@ def get_image(size, back_color, text_color, type, text):
     r = requests.get(url)
     return r.content if r.status_code == 200 else ""
     
-def gen_js_file2(images, size, types, file_path):
+def gen_js_file(images, size, types, file_path):
     with open(file_path, 'w') as f:
         params = gen_params(images)
         f.write("var photos = {\n")
@@ -48,23 +47,7 @@ def gen_js_file2(images, size, types, file_path):
             f.write("\n\t],\n") if i < (len(types) - 1) else f.write("\n\t]\n")
         
         f.write("};")
-        print params
-        
-def gen_js_file(images, size, type, file_path):
-    with open(file_path, 'w') as f:
-        f.write("var photosArr = [\n")
-        c = Color()
-
-        for i in range(0, images):
-            url = "http://dummyimage.com/%d/%s/%s.%s&text=%s" % (size, c.rnd_color(), c.contrast(), type, gen_id())
-            print url
-            r = requests.get(url)          
-            temp = b64encode(r.content) if r.status_code == 200 else ""
-            f.write("\t\"%s\"" % temp)
-            f.write(",\n") if i < (images - 1) else f.write("\n];\n\n")
-        
-        f.write("var photosType = '%s';\n" % type.upper())
-
+    
 if __name__ == "__main__":
-    #gen_js_file(images, size, type, file_path)
-    gen_js_file2(images, size, types, file_path)
+    gen_js_file(images, size, types, file_path)
+
