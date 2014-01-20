4c8818046c0f63bc45b525e0a772960a6317e761
sszachow <sszachow@gmail.com>
2014-01-20 12:47:27 +0100

    added dirty hack for disabling the button during vcf file generation

--- js/main.js
+++ js/main.js
@@ -6,21 +6,28 @@ var indexChars = numbers + letters.toUpperCase();
 var fileNameBase = "vcf";
 
 $(document).ready(function() {
-    var imgType = setDropDown('img_type', 'img_type', 'span#include_text', photos);
-        
-    $('button#generate').click(function() {
+    var $imgType = setDropDown('img_type', 'img_type', 'span#include_text', photos);
+    var $genButton = $('button#generate');
+    
+    $genButton.click(function() {
         var cntcts = parseInt($("input[id=contacts]").val());
         var start = parseInt($("input[id=start]").val());
-
+        
         if(validFields(cntcts, start)) {
             var names = new Names();
             var putPhotos = $('#photos').is(':checked');
-            var photosType = imgType.val();
-            var vcfStr = vcfAsStr(cntcts, start, names, photos, photosType, putPhotos);
-            var fName = fileName(cntcts, putPhotos, photosType);
-            
-            saveAs(new Blob([vcfStr], {type: "text/plain;charset=utf-8"}), fName);
-            vcfStr = null;
+            var photosType = $imgType.val();
+                        
+            $genButton.animate({
+                disabled: true
+            }, 100, function() {
+                var vcfStr = vcfAsStr(cntcts, start, names, photos, photosType, putPhotos);
+                var fName = fileName(cntcts, putPhotos, photosType);
+
+                saveAs(new Blob([vcfStr], {type: "text/plain;charset=utf-8"}), fName);
+                vcfStr = null;
+                $genButton.prop('disabled', false);
+            });            
         } else {
             window.alert("Please provide a number in range <" + minCntcts + ", " + maxCntcts + ">.");
         }
